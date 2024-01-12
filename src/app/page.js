b4435a0f5ddd6e2e2f346aaeaaa6fc37eacc38a6
Sameer_Raturi <sameerraturi2001@gmail.com>
2024-01-12 18:52:36 +0530

    flex and grid

--- src/app/page.js
+++ src/app/page.js
@@ -4,10 +4,28 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <div>
-      <h1 className="text-white bg-green-600 border-4 border-blue-200">Hello world!</h1>
+      <h1 className="text-white bg-green-600 border-4 border-blue-200">
+        Hello world!
+      </h1>
       <h1 className="text-white bg-sameer">Hello world!</h1>
 
-      <h1 className="text-white bg-sameer underline decoration-slate-100">Hello world!</h1>
+      <h1 className="text-white bg-sameer underline decoration-slate-100">
+        Hello world!
+      </h1>
+      <div className="flex items-baseline">
+        <div className="pt-2 pb-6 bg-sky-500"></div>
+        <div className="pt-8 pb-12 bg-red-500">02</div>
+        <div className="pt-12 pb-4 bg-green-500">04</div>
+      </div>
+
+      <div className="grid md:grid-col-2 gap-4 lg:grid-cols-3">
+        <div className="p-6 rounded-lg bg-sky-500">1</div>
+        <div className="p-6 rounded-lg bg-sky-500">2</div>
+        <div className="p-6 col-span-2 rounded-lg bg-red-500">3</div>
+        <div className="p-6 rounded-lg bg-sky-500">4</div>
+        <div className="p-6 rounded-lg bg-sky-500">5</div>
+        <div className="p-6 rounded-lg bg-sky-500">6</div>
+      </div>
     </div>
   );
 }
