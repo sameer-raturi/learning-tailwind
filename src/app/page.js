93c2269bb9358659c4c050a9378ad062145f43e6
Sameer_Raturi <sameerraturi2001@gmail.com>
2024-01-13 19:10:08 +0530

    design system

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,7 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <div>
-      <h1 className="text-white bg-green-600 border-4 border-blue-200">
+      {/* <h1 className="text-white bg-green-600 border-4 border-blue-200">
         Hello world!
       </h1>
       <h1 className="text-white bg-sameer">Hello world!</h1>
@@ -25,6 +25,26 @@ export default function Home() {
         <div className="p-6 rounded-lg bg-sky-500">4</div>
         <div className="p-6 rounded-lg bg-sky-500">5</div>
         <div className="p-6 rounded-lg bg-sky-500">6</div>
+      </div> */}
+      <h1>This is a title</h1>
+      <h2>This is a subtitle</h2>
+      <p>this is a paragraph</p>
+      <a>this is an anchor tag</a>
+      <div>
+        <button>this is a button</button>
+      </div>
+      <div>
+        <input type="text" />
+      </div>
+      <div>
+        <input type="checkbox" id="checkbox" />
+        <label for="checkbox">Checkbox</label>
+      </div>
+      <div>
+        <select>
+          <option>option 1</option>
+          <option>option 2</option>
+        </select>
       </div>
     </div>
   );
